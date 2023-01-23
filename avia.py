e29c45e894e6726215cb5a7e6079e053168025d6
newprogrammer01 <jasurbekpirnazarov09gmail.com>
2023-01-23 08:04:17 +0500

    el

---
+++ avia.py
@@ -0,0 +1,28 @@
+"""
+Aviachiptalarga buyurtmalar haqidagi joriy ma’lumotlarni taqdim 
+etuvchi dastur tuzing. Buyurtmalar quyidagi ma’lumotlardan iborat:
+- borish manzili;
+- parvoz raqami;
+- yo’lovchi ismi sharifi;
+- parvoz qilish sanasi.
+Dastur quyidagilarni bajarishi kerak:
+1. Barcha buyurtmalarni daraxt ko’rinishida saqlash;
+2. Buyurtma qo’shish va mavjud buyurtmani o’chirish;"""
+
+avia={
+    'borish manzili':"Amerika",
+    'parvoz raqami': 18,
+    'yulovchi ismi sharifi':'Yarashov Eldorjon ',
+    'parvoz qilish sanasi':'30.02.2023',
+
+     'borish manzili':"Malayziya",
+    'parvoz raqami': 1820,
+    'yulovchi ismi sharifi':'Eshqobilov Kamollddin ',
+    'parvoz qilish sanasi':'3.02.2023',
+
+     'borish manzili':"Kanada",
+    'parvoz raqami': 15,
+    'yulovchi ismi sharifi':'Karimov Sherzod ',
+    'parvoz qilish sanasi':'30.02.2023'
+}
+print(avia)
