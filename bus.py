4876a501a6e25b6e34e6e641a78a10d4463cf1b4
newprogrammer01 <jasurbekpirnazarov09gmail.com>
2023-01-23 07:25:58 +0500

    bus.py

---
+++ bus.py
@@ -0,0 +1,23 @@
+"""Avtobuslar parkida mavjud avtobuslar 
+haqidagi ma’lumotlarni qayta ishlovchi dastur tuzing. 
+Har bir avtobus haqida quyidagi ma’lumotlar mavjud:
+- avtobus raqami;
+- haydovchining ismi sharifi;
+- yo’nalish (marshrut) raqami."""
+bus1={
+    'Avtobus raqami':66,
+    'Haydovchining ismi va sharifi':"Eshqobilov Kamoliddin",
+    'yunalishi':'Yangi bozor,Dagbetiskaya, Hokimiyat, Gum, Dinamo stadioni, Pavarot, Hazora, Dustlik'
+}
+bus2={
+    "Avtobus raqami":37,
+    "Haydovchining ismi , sharifi":"Jurayov Kamronbek",
+    'yunalishi':"Chortut, Dustlik, Hazora, Tramatalogiya, vagzal, Oltin Samarqand, Dagbetiskaya,Siyob bozor, Giofezika,Avtovagzal"
+
+}
+print(bus1)
+print(bus2)
+"""{'Avtobus raqami': 66, 'Haydovchining ismi va sharifi': 'Eshqobilov Kamoliddin', 'yunalishi': 'Yangi bozor,Dagbetiskaya, Hokimiyat, Gum, Dinamo stadioni, Pavarot, Hazora, Dustlik'}
+{'Avtobus raqami': 37, 'Haydovchining ismi , sharifi': 'Jurayov Kamronbek', 'yunalishi': 'Chortut, Dustlik, Hazora, Tramatalogiya, vagzal, Oltin Samarqand, Dagbetiskaya,Siyob bozor, Giofezika,Avtovagzal'}
+"""
+#biz bu dasturga qushimchalar qushib kengaytrib ketishimiz mumkin, bizga kerakli yunalishni haydovchisi, yoki raqami orqali topib olaolamiz
